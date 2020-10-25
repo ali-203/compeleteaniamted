4c39c1d6c763c4ac9f17c25070582e83c3981e42
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-26 01:07:27 +0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Home";
 import About from  "./About";
 import Contact from "./Contact";
 import Service from "./Service";
+import Navbar from "./Navbar";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 
 const App = () => {
       return (
 <>
+<Navbar />
 <Switch>
 <Route exact path="/"  component={Home} />
 <Route exact path="/about" component={About} />
