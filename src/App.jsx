de9562fbf19f4cf10be69911637427b27e524f90
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-26 00:14:30 +0500

    add exact  path

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Home from "./Home";
 import About from  "./About";
 import Contact from "./Contact";
 import Service from "./Service";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 
 const App = () => {
       return (
 <>
 <Switch>
-<Route path="/"  component={Home} />
-<Route path="about" component={About} />
-<Route path="service" component={Service} />
-<Route path="contact" component={Contact} />
+<Route exact path="/"  component={Home} />
+<Route exact path="/about" component={About} />
+<Route exact path="/service" component={Service} />
+<Route exact path="/contact" component={Contact} />
+<Redirect to="/" />
 </Switch>
 </>
       );
