0e131e8ce84522f75ecc0869c92d1ec2ef4ca0d5
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-29 03:32:50 +0500

    css

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -36,26 +36,26 @@ return (
     <div className="row">
         <div className="col-md-6 col-10 mx-auto">
             <form onSubmit={formSubmit}>
-            <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label"> 
+            <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label"> 
   FullName
    </label>
   <input
    type="text"
-    class="form-control"
+    className="form-control"
      id="exampleFormControlInput1"
      name="name"
      value={data.fullname}
      onChange={InputEvent}
       placeholder="Enter your name"/>
 </div>
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">
+<div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">
   Phone
   </label>
   <input
    type="number"
-    class="form-control" 
+    className="form-control" 
     id="exampleFormControlInput1"
     name="phone"
      value={data.phone}
@@ -63,11 +63,11 @@ return (
 
      placeholder="name@example.com"/>
 </div>
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+<div classNameName="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
   <input
    type="email" 
-   class="form-control" 
+   className="form-control" 
    id="exampleFormControlInput1"
    name="email"
      value={data.email}
@@ -77,11 +77,11 @@ return (
     placeholder="name@example.com"/>
 </div>
 
-<div class="mb-3">
+<div className="mb-3">
   <label for="exampleFormControlTextarea1" 
-  class="form-label"> Message </label>
+  className="form-label"> Message </label>
   <textarea
-   class="form-control" 
+   className="form-control" 
   id="exampleFormControlTextarea1" 
   rows="3"
   name="msg"
