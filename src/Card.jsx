c37cafa884b9602631bef4b1382bc73a5fbf320a
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-01-03 22:57:05 +0500

    csupdated css

--- src/Card.jsx
+++ src/Card.jsx
@@ -14,7 +14,7 @@ return (
     <h5 className="card-title font-weight-bold">{props.title}</h5>
     <p className="card-text">
     Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <NavLink to="" className="btn btn-primary">Go somewhere</NavLink>
+    <NavLink to="" className="btn btn-primary">Go Somewhere</NavLink>
   </div>
 </div>
 
